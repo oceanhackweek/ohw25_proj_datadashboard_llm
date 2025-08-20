d2bbf848ed267b84c02dbe61207b084304567746
Default Jupyter user <aidan@axiomalaska.com>
2025-08-20 21:33:52 +0000

    added Dataset and Datasets class

---
+++ contributor_folders/aidan/dataset.py
@@ -0,0 +1,35 @@
+from typing import Optional, List
+from pydantic import BaseModel
+
+class TemporalBounds(BaseModel):
+    start_time: str
+    end_time: str
+
+class SpatialBounds(BaseModel):
+    min_lat: float
+    min_lon: float
+    max_lat: float
+    max_lon: float
+
+class Access(BaseModel):
+    platform: str
+    path: str
+
+class Variable(BaseModel):
+    standard_name: str
+    description: str
+
+class Variables(BaseModel):
+    variables: List[Variable]
+
+
+class Dataset(BaseModel):
+    name: str
+    description: str
+    temporal_bounds: TemporalBounds
+    spatial_bounds: SpatialBounds
+    variables: Variables
+    access: Access
+
+class DatasetCollection(BaseModel):
+    datasets: List[Dataset]
