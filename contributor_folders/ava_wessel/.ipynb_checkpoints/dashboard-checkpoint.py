590aa17ee56dfddb7225889edd530a33be4c762b
awessel3 <wessel.ava@gmail.com>
2025-08-21 22:18:02 +0000

    workingish dashboard

--- contributor_folders/ava_wessel/.ipynb_checkpoints/dashboard-checkpoint.py
+++ contributor_folders/ava_wessel/.ipynb_checkpoints/dashboard-checkpoint.py
@@ -8,74 +8,114 @@ app = marimo.App(width="medium")
 def _():
     import marimo as mo
     import pandas as pd 
-    import folium
-    from folium.plugins import Draw
     import geopandas as gpd
+    import openlayers as ol
     from shapely.geometry import box
-    from IPython.display import display
-
+    import os
     from langchain_community.vectorstores import Chroma
     from langchain.prompts import ChatPromptTemplate
     from langchain.chains import ConversationalRetrievalChain
     from langchain.schema import Document
     from langchain_openai import ChatOpenAI
     from langchain_core.output_parsers import StrOutputParser
-    return ChatOpenAI, ChatPromptTemplate, Draw, StrOutputParser, folium, mo
+
+    current_directory = os.getcwd()
+    print(current_directory)
+    return ChatOpenAI, ChatPromptTemplate, StrOutputParser, mo, ol
 
 
 @app.cell
-def _(mo):
-    mo.md("<h1>Data Dashboard with Chatbot!").center()
+def _():
+    from dotenv import load_dotenv
+    load_dotenv()
     return
 
 
 @app.cell
 def _(mo):
-    mo.md("<h0> OceanHackWeek 2025").center()
-    return
-
-
-@app.cell
-def _(Draw, folium):
-    m = folium.Map(location=[0, 0], zoom_start=2)
-    draw1 = Draw(export= True,  filename='map_selection.geojson',
-        draw_options={
-            'polyline': False,
-            'polygon': False,
-            'circle': False,
-            'circlemarker': False,
-            'marker': False,
-            'rectangle': True
-        },
-        edit_options={'edit': False}
+    mo.md(
+        """
+    <div style="display: flex; align-items: center; justify-content: center; gap: 12px;">
+        <img src="https://oceanhackweek.org/_static/logo.png" 
+             alt="Logo" width="50" height="50" style="border-radius: 0%;">
+        <h1 style="font-family: Arial, sans-serif; font-size: 36px; color: black; margin: 0; font-weight: bold">
+            Data Dashboard with Chatbot!
+        </h1>
+    </div>
+    """
     )
-    draw1.add_to(m)
-    m
     return
 
 
 @app.cell
-def _(ChatOpenAI, ChatPromptTemplate, StrOutputParser, mo):
-    def my_model(messages, config):
-        HF_TOKEN = "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"
+def _(mo):
+ 
+    return
 
-        import pandas as pd
-        rectangles_df = pd.DataFrame(columns=['southwest', 'northeast'])
 
-    # Example function to update rectangles (hooked to ipyleaflet DrawControl)
-    def add_rectangle(sw, ne):
-        rectangles_df.loc[len(rectangles_df)] = [sw, ne]
+@app.cell
+def _(ol):
+    m = ol.MapWidget()
+    m.add_click_interaction()
+    return (m,)
+
+
+@app.cell
+def _(m, mo):
+    widget = mo.ui.anywidget(m)
+    widget
+    return (widget,)
+
+
+@app.cell
+def _(mo):
+    mo.md("""<h0>Your selected Data Boundaries:""")
+    return
+
+
+@app.cell
+def _(mo, widget):
+    map_data = widget.value["view_state"]
+    map_frame = map_data['extent']
+    mo.md(
+        f"Longitude min: {round(map_frame[0], 2)}, Longitude max: {round(map_frame[2], 2)}  \n"
+        f"Latitude min: {round(map_frame[1], 2)}, Latitude max: {round(map_frame[3], 2)}"
+    )
+    return
+
+
+@app.cell
+def _(mo):
+    text_area = mo.ui.text_area(placeholder="Enter HF Token ...")
+    text_area
+    return (text_area,)
+
+
+@app.cell
+def _(text_area):
+    user_key = text_area.value.strip()
+    return (user_key,)
+
+
+@app.cell
+def _(ChatOpenAI, ChatPromptTemplate, StrOutputParser, mo, user_key, widget):
+    def my_model(messages, widget):
+        HF_TOKEN = user_key
+
+        map_frame = widget.value["view_state"]["extent"]
+        min_lon, min_lat, max_lon, max_lon = map_frame
 
         llm = ChatOpenAI(
             base_url="https://router.huggingface.co/v1",
             api_key=HF_TOKEN,
             model="openai/gpt-oss-20b:fireworks-ai"
         )
+        question = messages[-1].content
 
         prompt = ChatPromptTemplate.from_messages([
-            ("system", "You are an AI assistant. "
-                       "You have access to a map selection polygon "
-                       "that the user can draw."),
+            ("system", "You are an AI assistant that will answer questions about the area selected in the map."
+                       f"The current map boundaries are: {map_frame} that the user can draw."
+            f"users can ask what current map boundaries are provided by {min_lon, min_lat, max_lon, max_lon}"),
             ("human", "{question}")
         ])
 
@@ -83,11 +123,9 @@ def _(ChatOpenAI, ChatPromptTemplate, StrOutputParser, mo):
 
         response = chain.invoke({"question": "{question}"})
 
-
-
         return response
 
-    mo.ui.chat(my_model)
+    mo.ui.chat(lambda messages: my_model(messages, widget))
     return
 
 
--- contributor_folders/ava_wessel/dashboard.py
+++ contributor_folders/ava_wessel/dashboard.py
@@ -10,6 +10,7 @@ def _():
     import pandas as pd 
     import geopandas as gpd
     import openlayers as ol
+    from shapely.geometry import box
     import os
     from langchain_community.vectorstores import Chroma
     from langchain.prompts import ChatPromptTemplate
@@ -17,39 +18,24 @@ def _():
     from langchain.schema import Document
     from langchain_openai import ChatOpenAI
     from langchain_core.output_parsers import StrOutputParser
+
     current_directory = os.getcwd()
     print(current_directory)
+    from dotenv import load_dotenv
     return ChatOpenAI, ChatPromptTemplate, StrOutputParser, mo, ol
 
 
 @app.cell
-def _():
-    from dotenv import load_dotenv
-    load_dotenv()
-    return
-
-
-@app.cell
 def _(mo):
-    mo.md(
-        """
-    <div style="display: flex; align-items: center; justify-content: center; gap: 12px;">
+    mo.callout(mo.md("""
+    <div style="display: flex; align-items: center; justify-content: center; gap: 10px;">
         <img src="https://oceanhackweek.org/_static/logo.png" 
              alt="Logo" width="50" height="50" style="border-radius: 0%;">
-        <h1 style="font-family: Arial, sans-serif; font-size: 36px; color: #004080; margin: 0; font-weight: bold">
-            Data Dashboard with Chatbot!
+        <h1 style="font-family: Arial, subpixel-antialiased; font-size: 50px; color: black; margin: 0; font-weight: bold">
+            Data Dashboard with Chatbot
         </h1>
     </div>
-    """
-    )
-    return
-
-
-@app.cell
-def _(mo):
-    mo.md("""
-    <h1 style = "font-family:Arial, sans-serif; font-size:15px;color: #00408;  font-weight: bold"> OceanHackWeek 2025
-    """).center()
+    """), kind = 'info')
     return
 
 
@@ -69,17 +55,52 @@ def _(m, mo):
 
 @app.cell
 def _(mo):
-    mo.md("""<h0>Your selected Data Boundaries:""")
+    mo.md(
+        """
+    <div style="gap: 12px;">
+        <h1 style="font-family: Arial, sans-serif; font-size: 17px; color: '#2791F5'; margin: 0; font-weight: bolder">
+            Your Selected Data Boundaries:
+        </h1>
+    </div>
+    """
+    )
     return
 
 
 @app.cell
-def _(mo, widget):
+def _(widget):
+    x, y = 0,0
+    if len(widget.value["clicked"]) > 0 :
+        point_selected = widget.value["clicked"]['coordinate']
+        x, y = point_selected
+
+    return x, y
+
+
+@app.cell
+def _(mo, widget, x, y):
     map_data = widget.value["view_state"]
     map_frame = map_data['extent']
     mo.md(
-        f"Longitude min: {round(map_frame[0], 2)}, Longitude max: {round(map_frame[2], 2)}  \n"
-        f"Latitude min: {round(map_frame[1], 2)}, Latitude max: {round(map_frame[3], 2)}"
+        f"<span style='font-size:16px'><b>Longitude min:</b> {round(map_frame[0], 2)}, "
+        f"<b>Longitude max:</b> {round(map_frame[2], 2)}</span><br>"
+        f"<span style='font-size:16px'><b>Latitude min:</b> {round(map_frame[1], 2)}, "
+        f"<b>Latitude max:</b> {round(map_frame[3], 2)}</span><br>"
+        f"<span style='font-size:18px; color:darkblue'><b>Point Selected:</b> {round(x, 2)}, {round(y, 2)}</span>"
+    )
+    return
+
+
+@app.cell
+def _(mo):
+    mo.md(
+        r"""
+    <div style="gap: 12px;">
+        <h1 style="font-family: Arial, sans-serif; font-size: 15px; color: '#2791F5'; margin: 0; font-weight: bolder">
+            Please provide HF Token to run the Chatbot:
+        </h1>
+    </div>
+    """
     )
     return
 
@@ -98,38 +119,33 @@ def _(text_area):
 
 
 @app.cell
-def _(user_key):
-    user_key
-    return
-
-
-@app.cell
 def _(ChatOpenAI, ChatPromptTemplate, StrOutputParser, mo, user_key, widget):
     def my_model(messages, widget):
         HF_TOKEN = user_key
 
         map_frame = widget.value["view_state"]["extent"]
+        point_selected = widget.value['clicked']['coordinate']
+        min_lon, min_lat, max_lon, max_lon = map_frame
+        x, y = point_selected
 
         llm = ChatOpenAI(
             base_url="https://router.huggingface.co/v1",
             api_key=HF_TOKEN,
             model="openai/gpt-oss-20b:fireworks-ai"
         )
-        question = messages[-1]["content"]
+        question = messages[-1].content
 
         prompt = ChatPromptTemplate.from_messages([
-            ("system", "You are an AI assistant specializing in ocean data. "
-                       "he current map boundaries are: {map_frame}."
-                       "that the user can draw."),
+            ("system", "You are an AI assistant that will answer questions about the area selected in the map."
+                       f"The current map boundaries are: {map_frame} that the user can draw."
+            f"users can ask what current map boundaries are provided by {min_lon, min_lat, max_lon, max_lon}"
+            f"users can ask you about the specific coordinates selected {point_selected}"),
             ("human", "{question}")
         ])
 
         chain = prompt | llm | StrOutputParser()
 
-        response = chain.invoke({"question": "{question}"},
-                               {'map boundaries': ""})
-
-
+        response = chain.invoke({"question": "{question}"})
 
         return response
 
